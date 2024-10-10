b2a94cfab6a96e88b3bf2f230dc6dd42d29392f7
D-Bars <awotik26@gmail.com>
2024-10-10 18:05:14 +0200

    fixed description from archive-services

--- archive-services.php
+++ archive-services.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
 
-
 <?php if (have_posts()): ?>
     <div class="archive__services__block">
         <h1 class="h1__archive__pages"><?php _e('All Services', 'hi-tech'); ?></h1>
         <?php while (have_posts()):
             the_post(); ?>
+            <?php $trimmed_content = wp_trim_words(get_the_content(), 20, '...'); ?>
             <div class="archive__services__item">
                 <div class="archive__services__item__wrapper__img"><img src="<?php the_post_thumbnail_url('full'); ?>"
                         alt="<?php the_title(); ?>"></div>
                 <a class="archive__services__item__title"
                     href="<?php the_permalink(); ?>"><?php get__localize__title(get_the_ID()); ?></a>
                 <div class="archive__services__item__description">
-                    <?php get__localize__content(get_the_ID()); ?>
+                    <?php printf(__('%s', 'hi-tech'), $trimmed_content); ?>
                 </div>
                 <div class="services__item__read__more__box">
                     <div class="services__item__read__more"><a
