d5c2ee47328cf7a8b598b46806075ccc2f85af79
D-Bars <awotik26@gmail.com>
2024-10-10 16:51:11 +0200

    added about-us page

--- page-about-us.php
+++ page-about-us.php
@@ -1,1 +1,11 @@
-123
+<?php get_header(); ?>
+
+<div class="about__us__block">
+    <div class="about__us__content__box">
+        <div class="about__us__content__title"><?php _e('About Us', 'hi-tech'); ?></div>
+        <div class="about__us__content"><?php _e('Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est ea blanditiis rem molestiae excepturi dolorum esse quos quod libero iusto laborum quis, quae natus suscipit, soluta nam? Natus, sapiente exercitationem.', 'hi-tech'); ?></div>
+    </div>
+    <div class="about__us__wrapper__img"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/about-us.jpg" alt="AboutUs"></div>
+</div>
+
+<?php get_footer(); ?>
